09a7aec92e39e5e69a9902ab3d83b84d41a74104
Sofia Simola <2265780s@student.gla.ac.uk>
2018-12-05 13:03:53 +0000

    Coloured the selected part

--- ScriptSpeare/javascript/scriptmemory.js
+++ ScriptSpeare/javascript/scriptmemory.js
@@ -1,1 +1,8 @@
-// JavaScript source code
+function ScriptMemory(media, script) {
+	media.addTimeUpdate(function(event){
+		media = event.data
+		if (media.looping) {
+			if (media.getTime() > media.loopEnd || media.getTime() < media.loopStart) media.setTime(media.loopStart);
+		}
+    });
+};

93cc7310b9566bfb129bb075afd6e17bb15dbd75
Sofia Simola <2265780s@student.gla.ac.uk>
2019-01-24 17:07:33 +0000

    Functioning replay button

--- ScriptSpeare/javascript/ui_elements.js
+++ ScriptSpeare/javascript/ui_elements.js
@@ -1,4 +1,14 @@
 //Document for functions which change the appearance of UI element
+$(document).ready(function(){
+	checkEnd = function(event) {
+		if (event.data.getTime() >= event.data.getDuration()) {
+			$("#playbutton")[0].className = "fas fa-redo bar-button"
+		};
+	};
+	console.log(media);
+	media.addTimeUpdate(checkEnd, media);
+});
+
 
 function togglePauseUI(target, media) {
 	playIcon = "fas fa-play-circle bar-button";
@@ -8,5 +18,5 @@ function togglePauseUI(target, media) {
 		target.className = pauseIcon;
 	} else {
 		target.className = playIcon;
-	}
-} 
+	};
+};

15b39122e379a57d0302ec466d39af42107260c4
Ivan Delev <2262800d@student.gla.ac.uk>
2019-01-29 12:47:38 +0000

    Delete duplicate and unneccessary files

--- ScriptSpeare/javascript/scriptmemory.js
+++
@@ -1,8 +0,0 @@
-function ScriptMemory(media, script) {
-	media.addTimeUpdate(function(event){
-		media = event.data
-		if (media.looping) {
-			if (media.getTime() > media.loopEnd || media.getTime() < media.loopStart) media.setTime(media.loopStart);
-		}
-    });
-};

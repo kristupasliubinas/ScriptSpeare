479ef592ea7cac69056b891d94f6fc71d2cf88f6
Sofia Simola <2265780s@student.gla.ac.uk>
2019-02-10 13:59:00 +0000

    Turns JSON into somewhat clickable html

--- ScriptSpeare/static/javascript/parser.js
+++ ScriptSpeare/static/javascript/parser.js
@@ -9,41 +9,53 @@ function render(ret) {
 
     var currentOffset = 0;
 
+	html = '';
+
     wds.forEach(function(wd) {
         if(wd.case == 'not-found-in-transcript') {
             var txt = ' ' + wd.word;
-            var $plaintext = document.createTextNode(txt);
-            $trans.appendChild($plaintext);
+			html += txt;
             return;
         };
 
         // Add non-linked text
         if(wd.startOffset > currentOffset) {
             var txt = transcript.slice(currentOffset, wd.startOffset);
-            var $plaintext = document.createTextNode(txt);
-            $trans.appendChild($plaintext);
+			html += txt;
             currentOffset = wd.startOffset;
         };
 
-        var $wd = document.createElement('span');
-        var txt = transcript.slice(wd.startOffset, wd.endOffset);
-        var $wdText = document.createTextNode(txt);
-        $wd.appendChild($wdText);
-        wd.$div = $wd;
-        if(wd.start !== undefined) {
-            $wd.className = 'success';
-        };
-		$wd.setAttribute("start", wd.start);
-		$wd.setAttribute("end", wd.end);
-        $wd.onclick = function() {state.click($(this))};
-        $trans.appendChild($wd);
-        currentOffset = wd.endOffset;
+		if (['h1', 'h2', 'br', 'b', 'i'].includes(wd.word)) {
+			var txt = transcript.slice(wd.startOffset, wd.endOffset);
+			html += txt;
+			currentOffset = wd.endOffset;
+		} else {
+			var $wd = document.createElement('span');
+			var txt = transcript.slice(wd.startOffset, wd.endOffset);
+			var $wdText = document.createTextNode(txt);
+			$wd.appendChild($wdText);
+			wd.$div = $wd;
+			if(wd.start !== undefined) {
+				$wd.className = 'success';
+			};
+			$wd.setAttribute("start", wd.start);
+			$wd.setAttribute("end", wd.end);
+			html += $wd.outerHTML;
+			currentOffset = wd.endOffset;
+		};
     });
 
     var txt = transcript.slice(currentOffset, transcript.length);
-    var $plaintext = document.createTextNode(txt);
-    $trans.appendChild($plaintext);
+	html += txt;
     currentOffset = transcript.length;
+	$trans.innerHTML = html;
+
+	$trans.childNodes.forEach(function(element) {
+		if (element.nodeName == "SPAN") {
+			element.onclick = function() {state.click($(element))};
+		};
+	});
+	console.log($trans.innerHTML);
 };
 
 $(document).ready(function(){

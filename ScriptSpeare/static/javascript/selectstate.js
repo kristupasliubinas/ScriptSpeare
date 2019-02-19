dd3660a2c882e27b808d38f85d11d94e95885346
Sofia Simola <2265780s@student.gla.ac.uk>
2019-02-19 20:53:29 +0000

    Made fixes to the looping

--- ScriptSpeare/static/javascript/selectstate.js
+++ ScriptSpeare/static/javascript/selectstate.js
@@ -16,11 +16,14 @@ $(document).ready(function(){
 		if (state.constructor.name == "selectState") {
 			//TODO error checking
 			selection = window.getSelection();
-			if (!selectionHasText(selection)) return;
+			// 4  for selecting forwards, 2 for backwards
+			position = selection.anchorNode.compareDocumentPosition(selection.focusNode);
+			span1 = $(getParentSpan(selection.anchorNode, position % 4));
+			span2 = $(getParentSpan(selection.focusNode, (position + 2 ) % 4));
+
+			if (!(span1 && span2)) return;
 
 			resetColour();
-			span1 = $(getParentSpan(selection.anchorNode));
-			span2 = $(getParentSpan(selection.focusNode));
 			t1 = parseTime(span1.attr("start"));
 			t2 = parseTime(span2.attr("start"));
 			if (t1 > t2) {
@@ -35,13 +38,15 @@ $(document).ready(function(){
 	});
 });
 
-//TODO Change this to accept selection with one part outside of the text
-function selectionHasText(selection) {
-	console.log(getParentSpan(selection.anchorNode) && getParentSpan(selection.focusNode));
-	return (getParentSpan(selection.anchorNode) && getParentSpan(selection.focusNode));
-};
-
-function getParentSpan(element) {
+// Position is 0 for being the fist node in the selection, 2 for being the last.
+function getParentSpan(element, position) {
+	if (element.parentElement.className == 'col-9 script' && element.nodeName != "SPAN") {
+		if (position == 2) {
+			return getParentSpan(element.nextSibling, position);
+		} else {
+			return getParentSpan(element.previousSibling, position);
+		};
+	};
 	while (element.nodeName != "SPAN") {
 		element = element.parentElement;
 		if (!element) return null;

04730f8405bebced611c872bded16bc94707fd1a
Ivan Delev <2262800d@student.gla.ac.uk>
2019-03-03 17:51:43 +0000

    Removed models, db and unnecessary files

--- ScriptSpeare/ScriptSpeare/admin.py
+++ ScriptSpeare/ScriptSpeare/admin.py
@@ -1,17 +1,2 @@
 from django.contrib import admin
-from ScriptSpeare.models import Category,Play,Interpretation
-
-
-
-
-class CategoryAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug':('name',)}
-class PlayAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug':('title',)}
-class InterpretationAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug':('male_lead_name','year',)}
-	
-# Update the registration to include this customised interface
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Play,PlayAdmin)
-admin.site.register(Interpretation, InterpretationAdmin)
+#Admin Settings

a199754427aa3a7d410f611c816c07f883f21d17
Ivan Delev <2262800d@student.gla.ac.uk>
2019-03-15 13:28:36 +0000

    Removed irrlevent code in views.py, chnaged urls.py to accomodate changes

--- ScriptSpeare/ScriptSpeare/views.py
+++ ScriptSpeare/ScriptSpeare/views.py
@@ -55,7 +55,8 @@ def show_play(request, category, play, interp_id=0):
 			abbr='Julius'
 		else:
 			abbr=play
-		
+	
+	interp_id=int(interp_id)
 	url="https://1zct9uiole.execute-api.eu-west-2.amazonaws.com/prod/get-adaptation-list?category="+category+"&play="+abbr
 	response=requests.get(url)
 	all_interp=response.json()
@@ -104,68 +105,3 @@ def show_play(request, category, play, interp_id=0):
 		
 		
 	return render(request, 'ScriptSpeare/interpretation.html', context_dict)
-	
-def show_interpretation(request, category, play, interp_id):
-	context_dict={}
-	
-	
-	category = category
-	play=play
-	if category=='Comedy':
-		if play=='Merchant of Venice':
-			abbr='Merchant'
-		elif play=='A Midsummer Night\'s Dream':
-			abbr='Midsummer'
-		elif play=='Twelfth Night':
-			abbr='Twelfth'
-		else:
-			abbr=play
-	elif category=='Tragedy':
-		if play=='Romeo and Juliet':
-			abbr='Romeo'
-		elif play=='Julius Caesar':
-			abbr='Julius'
-		else:
-			abbr=play
-	url="https://1zct9uiole.execute-api.eu-west-2.amazonaws.com/prod/get-adaptation-list?category="+category+"&play="+abbr
-	response=requests.get(url)
-	all_interp=response.json()
-	for i in range(len(all_interp)-1):
-		if len(all_interp[i])!=6:
-			all_interp[i]=["","","","","",""]
-	
-	for el in all_interp:
-		el.append(str.replace(el[4],'_',' '))
-		el.append(str.replace(el[5],'_',' '))
-	id=0
-	for el in all_interp:
-		el.append(id)
-		id=id+1
-	interp_id=int(interp_id)
-	interp_id_max=len(all_interp)-1
-	interp=all_interp[interp_id]
-	if int(interp_id)==0:
-		prev=interp_id_max
-	else:
-		prev=interp_id-1
-			
-	if int(interp_id)==interp_id_max:
-		next=0
-	else:
-		next=interp_id+1
-			
-			
-	context_dict['all_interp']=all_interp
-	context_dict['interp']=interp
-	context_dict['interp_id']=interp_id
-	context_dict['play']=play
-	context_dict['abbr']=abbr
-	context_dict['prev']=prev
-	context_dict['next']=next
-	context_dict['category']=category
-		
-	
-		
-	
-	return render(request, 'ScriptSpeare/interpretation.html', context_dict)
-		
